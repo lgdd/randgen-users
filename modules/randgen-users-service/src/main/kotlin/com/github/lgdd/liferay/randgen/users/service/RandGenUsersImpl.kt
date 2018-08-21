45ea528d824c701dc6fc1452f2018cee0ff00dc8
lgdd <louis-guillaume.durand@liferay.com>
2018-08-21 12:36:43 +0200

    Update deprecated StringPool reference

--- modules/randgen-users-service/src/main/kotlin/com/github/lgdd/liferay/randgen/users/service/RandGenUsersImpl.kt
+++ modules/randgen-users-service/src/main/kotlin/com/github/lgdd/liferay/randgen/users/service/RandGenUsersImpl.kt
@@ -8,7 +8,7 @@ import com.liferay.portal.kernel.model.User
 import com.liferay.portal.kernel.service.UserLocalServiceUtil
 import com.liferay.portal.kernel.util.FileUtil
 import com.liferay.portal.kernel.util.PortalUtil
-import com.liferay.portal.kernel.util.StringPool
+import com.liferay.petra.string.StringPool;
 import org.apache.commons.io.IOUtils
 import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
